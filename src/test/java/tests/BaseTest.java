27fabc92fc8a7ac6f30fc1413a5d3b49fb2efb6e
Sagar Suresh Chaudhari <66245092+sagar-maker@users.noreply.github.com>
2026-09-19 21:31:35 +0530

    Update BaseTest.java
    Add Base functions

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,13 +1,42 @@
 package tests;
 
-import io.appium.java-client.pagefactory.AppiumFieldDecorator;
-import io.appium.java-client.windows.WindowsDriver;
-import io.appium.java-client.windows.WindowsElement;
-import org.openqa.selenium.By;
+import io.appium.java_client.windows.WindowsDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
+    // FIX: Removed generic types from the ThreadLocal assignment
+    private static final ThreadLocal<WindowsDriver> driverThread = new ThreadLocal<>();
+
+    public WindowsDriver getDriver() {
+        return driverThread.get();
+    }
+
+    @BeforeMethod
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("app", "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App");
+        capabilities.setCapability("deviceName", "WindowsPC");
+
+        // FIX: Replaced parameter initialization assignment
+        WindowsDriver driver = new WindowsDriver(new URL("http://127.0.0.1:4723"), capabilities);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driverThread.set(driver);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (getDriver() != null) {
+            getDriver().quit();
+            driverThread.remove();
+        }
+    }
 }
